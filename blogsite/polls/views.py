d053440969187513695e92c162aafde50c54686f
Solomon Adebayo <solomoniyi@gmail.com>
2013-06-16 17:58:32 -0700

    Python tutorial, Tutorial 1 to 3: for blog site, for getting back in python

---
+++ blogsite/polls/views.py
@@ -0,0 +1,19 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from polls.models import Poll
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)    
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)

ae4d9697dda5cfb7918f452542bc0f906b4d152e
Solomon Adebayo <solomoniyi@gmail.com>
2013-07-01 22:15:39 +0100

    recrated models ; tweets, location, reports and locationtypes tables in sqlite database

--- blogsite/epi/models.py
+++ blogsite/epi/models.py
@@ -2,14 +2,68 @@ from django.db import models
 
 # Create your models here.
 
-class Tweet(models.Model):
-    status = models.CharField(max_length=200)
-    owner = models.CharField(max_length=20)
+class LocationType(models.Model):
+    name = models.CharField(max_length=100)
+    
+    
+    class Meta:
+        verbose_name = "LocationType"
+    
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        db_table = "epi_locationtype"
+
+class Location(models.Model):
+    """A Location is technically a geopgraphical point (lat+long), but could be
+       used to represent a large area such as a city or country. It is recursive
+       via the _parent_ field, which can be used to create a hierachy (Country
+       -> County -> City) in combination with the _type_ field."""
     latitude  = models.DecimalField(max_digits=8, decimal_places=6, blank=True, null=True, help_text="The physical latitude of this location")
     longitude = models.DecimalField(max_digits=8, decimal_places=6, blank=True, null=True, help_text="The physical longitude of this location")
+    name = models.CharField(max_length=100, help_text="Name of location", db_index=True)
+    parent = models.ForeignKey("Location", related_name="children", null=True, blank=True,
+        help_text="The parent of this Location. It" +\
+                  "is expected that the parent will be of a different LocationType, although not enforced")
+    level = models.PositiveIntegerField(blank=True, default=0, db_index=True,  help_text="Two levels exist. 1, 2.")
+    locationtype = models.ForeignKey(LocationType, related_name="locations", blank=True, null=True)
 
+    class Meta:
+        db_table = "epi_location"
+
+class Tweet(models.Model):
+    """A Tweet represents a twitter message."""
+    text = models.CharField(max_length=200)
+    owner = models.CharField(max_length=20)
+    label = models.CharField(max_length=20)
+    usage = models.CharField(max_length=20)
+    source = models.CharField(max_length=20)
+    urlentity = models.CharField(max_length=20)
+    hashtagentity = models.CharField(max_length=20)
+    count = models.PositiveIntegerField()
+    location= models.ForeignKey(Location)
+    
     def __unicode__(self):
         return self.status
 
     def getStatus(self):
-    	pass
+        pass
+
+    class Meta:
+        db_table = "epi_tweet"
+
+
+class Reports(models.Model):
+    """This class models report types; Maps, Trend Charts, Visualization and Alerts graphs."""
+    report = models.CharField(max_length=20)
+    
+    
+    def __unicode__(self):
+        return self.status
+
+    def getStatus(self):
+        pass
+
+    class Meta:
+        db_table = "epi_reports"

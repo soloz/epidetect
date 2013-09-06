dccb901faf3c252b7de0d6c6ebd1fb340f6627ec
Solomon Adebayo <solomoniyi@gmail.com>
2013-09-06 14:54:31 +0100

    url redesign from local to web deployment compatible format

--- epidetect/urls.py
+++ epidetect/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^epiweb/', include('epiweb.urls', namespace="epiweb")),
+    url(r'^outbreaks/', include('epiweb.urls', namespace="epiweb")),
     url(r'^epiweb/documentclass', include('epiweb.urls', namespace="epiweb")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- epiweb/urls.py
+++ epiweb/urls.py
@@ -4,7 +4,7 @@ from epiweb import views
 
 urlpatterns = patterns('',
     url(r'^prototype$', views.formhandler, name='formhandler'),
-    url(r'^improved/(?P<disease>\w+)', views.ImprovedIndexView.as_view(), name='improved'),
+    url(r'^(?P<disease>\w+)', views.ImprovedIndexView.as_view()),
     url(r'^performance$', views.performance, name='performance'),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='details'),
